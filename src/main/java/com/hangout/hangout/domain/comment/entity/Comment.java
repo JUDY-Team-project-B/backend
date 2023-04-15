a200b76214ec5db3f1b5997fbcd6f7f4da055bc4
조성현 <agllwy116@naver.com>
2023-04-15 19:43:21 +0900

    feat : Comment 관련 Entity 생성

---
+++ src/main/java/com/hangout/hangout/domain/comment/entity/Comment.java
@@ -0,0 +1,57 @@
+package com.hangout.hangout.domain.comment.entity;
+
+import com.hangout.hangout.domain.post.entity.Post;
+import com.hangout.hangout.domain.user.entity.User;
+import com.hangout.hangout.global.common.domain.BaseEntity;
+import com.hangout.hangout.global.common.domain.Status;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "COMMENT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STATUS_ID")
+    private Status status; // 댓글에 상태는 하나만 존재해야 해서 OneToOne이 아닌가요?
+
+    // 40 ~ 48 : Self Join
+    @Column(name = "PARENT_ID")
+    private String parentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PARENT_ID", referencedColumnName = "COMMENT_ID", insertable = false, updatable = false)
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+    private List<Comment> children = new ArrayList<>();
+
+    @Lob
+    @Column(name = "CONTENT", nullable = false)
+    private String content;
+
+    @ColumnDefault("0")
+    private int reportCnt;
+    private String createdBy; // 유저 name 이것도 User 에서 가져오면 되는 것 같은데
+}
---
+++ src/main/java/com/hangout/hangout/domain/comment/entity/CommentLike.java
@@ -0,0 +1,32 @@
+package com.hangout.hangout.domain.comment.entity;
+
+import com.hangout.hangout.domain.user.entity.User;
+import com.hangout.hangout.global.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "COMMENT_LIKE")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentLike extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_LIKE_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user; // user와 조인을 해야 하는 이유가 무엇인가요?
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMMENT_ID")
+    private Comment comment; // one to one 이 아니라 Many to One 인 이유가 있나요?
+
+    @ColumnDefault("0")
+    private int likeCnt; // 추천 수
+}
