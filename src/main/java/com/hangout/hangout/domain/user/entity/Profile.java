9dd194417dd9d8c9a6fd04c593b614aefc16a9c4
Kim-Hye-Lin <k1h2fls@gmail.com>
2023-05-01 21:50:31 +0900

    refactor: User entity 통합
    profile, user entity -> user 통합
    - profile entity가 따로 필요가 없을 것 같아 우선 user entity로 통합했습니다.

--- src/main/java/com/hangout/hangout/domain/user/entity/Profile.java
+++ src/main/java/com/hangout/hangout/domain/user/entity/Profile.java
@@ -1,45 +1,55 @@
-package com.hangout.hangout.domain.user.entity;
-
-import com.hangout.hangout.global.common.domain.BaseEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Entity
-@Getter
-@Table(name = "PROFILE")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Profile extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "PROFILE_ID")
-    private Long id;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "USER_ID")
-    private User user;
-    @Column(name="NAME",nullable = false)
-    private String name;
-    @Column(name="NICKNAME",nullable = false)
-    private String nickname;
-    private String image; // 이미지의 URL 저장 // 리스트로 저장해야 하나?
-    private String email;
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-    private int age;
-
-    @Builder
-    public Profile(User user,String name, String nickname, String image
-            ,String email, Gender gender, int age) {
-        this.user =user;
-        this.name = name;
-        this.nickname = nickname;
-        this.image = image;
-        this.email = email;
-        this.gender= gender;
-        this.age = age;
-    }
-}
+//package com.hangout.hangout.domain.user.entity;
+//
+//import com.hangout.hangout.global.common.domain.BaseEntity;
+//import com.hangout.hangout.global.error.ResponseType;
+//import com.hangout.hangout.global.exception.InvalidFormatException;
+//import lombok.AccessLevel;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Getter
+//@Table(name = "PROFILE")
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor
+//@Builder
+//public class Profile extends BaseEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "PROFILE_ID")
+//    private Long id;
+//
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "USER_ID")
+//    private User user;
+//
+//    // todo 필요여부 논의
+////    @Column(nullable = false)
+////    private String name;
+//
+//    @Column(nullable = false, unique = true)
+//    private String nickname;
+//
+//    private String image;
+//
+//    @Enumerated(EnumType.STRING)
+//    private Gender gender;
+//
+//    private int age;
+//
+//    public void updateNickName(final String nickname) {
+//        validateNickname(nickname);
+//        this.nickname = nickname;
+//    }
+//
+//    private void validateNickname(final String nickname) {
+//        if (nickname.length() > 100) {
+//            throw new InvalidFormatException(ResponseType.INVALID_FORMAT);
+//        }
+//    }
+//}
--- src/main/java/com/hangout/hangout/domain/user/entity/User.java
+++ src/main/java/com/hangout/hangout/domain/user/entity/User.java
@@ -1,38 +1,98 @@
 package com.hangout.hangout.domain.user.entity;
 
 import com.hangout.hangout.global.common.domain.BaseEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.hangout.hangout.global.error.ResponseType;
+import com.hangout.hangout.global.exception.InvalidFormatException;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "USER")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class User extends BaseEntity {
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long id;
 
-    @Column(name = "EMAIL" , nullable = false)
+    @Column(nullable = false, unique = true)
     private String email; // 이메일(ID)
 
-    @Column(name = "PASSWORD", nullable = false)
+    @Column(nullable = false)
     private String password; // 비밀번호
 
-    @Builder
-    public User(String email, String password) {
-        this.email=email;
-        this.password= password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    private String image;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private int age;
+
+    public void updateNickName(final String nickname) {
+        validateNickname(nickname);
+        this.nickname = nickname;
     }
+
     public void updatePassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    private void validateNickname(final String nickname) {
+        if (nickname.length() > 100) {
+            throw new InvalidFormatException(ResponseType.INVALID_FORMAT);
+        }
     }
 }
