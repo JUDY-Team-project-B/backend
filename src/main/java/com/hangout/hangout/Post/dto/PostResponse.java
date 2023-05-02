0dd253850b97a431b17a77f4a8622598e55a746b
조성현 <agllwy116@naver.com>
2023-05-02 10:34:04 +0900

    feat: 특정 게시물 조회 시 클라이언트에 반환할 정보 클래스 생성

---
+++ src/main/java/com/hangout/hangout/Post/dto/PostResponse.java
@@ -0,0 +1,57 @@
+package com.hangout.hangout.Post.dto;
+
+import com.hangout.hangout.domain.post.entity.Post;
+import com.hangout.hangout.domain.post.entity.PostInfo;
+import com.hangout.hangout.domain.post.entity.PostTagRel;
+import com.hangout.hangout.domain.user.entity.Gender;
+import com.hangout.hangout.domain.user.entity.User;
+import com.hangout.hangout.global.common.domain.Status;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Set;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class PostResponse {
+    // 게시글 특정 게시물 조회 시 반환할 목록 클래스
+    // 추후 기능 만들고 추가해야 할 것 : Map, 좋아요, 신고, 조회수, 이미지
+    private final Long id;
+    private final String title;
+    private final String user;
+    private final String context;
+    private final Set<PostTagRel> tags;
+    private final Status status;
+    private final Gender travelGender;
+    private final String travelAge;
+    private final String travelAt;
+    private final int travelMember;
+    private Date travelDateStart;
+    private Date travelDateEnd;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public static PostResponse of(Post post) {
+        return PostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .user(post.getUser().getEmail())
+                .context(post.getContext())
+                .tags(post.getTags())
+                .status(post.getPostInfo().getStatus())
+                .travelGender(post.getPostInfo().getTravelGender())
+                .travelAge(post.getPostInfo().getTravelAge())
+                .travelAt(post.getPostInfo().getTravelAt())
+                .travelMember(post.getPostInfo().getTravelMember())
+                .travelDateStart(post.getPostInfo().getTravelDateStart())
+                .travelDateEnd(post.getPostInfo().getTravelDateEnd())
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
+                .build();
+    }
+}
