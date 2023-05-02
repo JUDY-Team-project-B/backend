2e9d06dfb6b14862b174969c77da1b094dabc0bf
조성현 <agllwy116@naver.com>
2023-05-02 14:21:58 +0900

    feat : 포스트 아이디로 조회할 시 삭제 여부를 검사해서 결과를 반환하도록 수정

--- src/main/java/com/hangout/hangout/Post/domain/repository/PostRepository.java
+++ src/main/java/com/hangout/hangout/Post/domain/repository/PostRepository.java
@@ -2,9 +2,12 @@ package com.hangout.hangout.Post.domain.repository;
 
 import com.hangout.hangout.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("select p from Post p where p.id = :postId and p.postInfo.status.id = 1")
     Optional<Post> findPostById(Long postId);
 }
