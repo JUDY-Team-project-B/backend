94a6e120e73a2006c409532c31dd150e73a3411c
조성현 <agllwy116@naver.com>
2023-05-16 01:34:10 +0900

    feat : 검색 조건 관련 Enum 및 검색 타입,키워드를 받는 DTO 객체 생성

---
+++ src/main/java/com/hangout/hangout/domain/post/dto/PostSearchRequest.java
@@ -0,0 +1,16 @@
+package com.hangout.hangout.domain.post.dto;
+
+import com.hangout.hangout.global.common.domain.entity.SearchType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@ToString
+public class PostSearchRequest {
+    private SearchType searchType;
+    private String searchKeyword;
+}
