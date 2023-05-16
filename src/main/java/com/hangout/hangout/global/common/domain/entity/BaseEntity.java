4cc0a445cd82995aa7bb91e1407278379d8aee7a
Kim-Hye-Lin <k1h2fls@gmail.com>
2023-05-16 22:12:21 +0900

    fix: update 시점에만 DB 저장되도록 수정

--- src/main/java/com/hangout/hangout/global/common/domain/entity/BaseEntity.java
+++ src/main/java/com/hangout/hangout/global/common/domain/entity/BaseEntity.java
@@ -1,14 +1,12 @@
 package com.hangout.hangout.global.common.domain.entity;
 
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
+import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -19,15 +17,16 @@ public abstract class BaseEntity {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
-
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
     }
 }
