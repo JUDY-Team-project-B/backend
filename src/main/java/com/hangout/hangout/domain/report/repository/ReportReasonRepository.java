e51aa62123dd9d72d1170c5f28f31a02155e5d3f
Kim-Hye-Lin <k1h2fls@gmail.com>
2023-05-24 21:28:04 +0900

    fix: delete unnecessary repository

--- src/main/java/com/hangout/hangout/domain/report/repository/ReportReasonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hangout.hangout.domain.report.repository;
-
-import com.hangout.hangout.domain.report.entity.ReportReason;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportReasonRepository extends JpaRepository<ReportReason, Long> {
-
-}
