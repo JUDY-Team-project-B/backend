1ff1fcd4d1751ee5b47798066deeef21683a8d59
BellYun <whd3558@naver.com>
2023-05-24 11:33:42 +0900

    refactor : 로그인 관련된 코드 삭제

--- src/main/java/com/hangout/hangout/global/config/WebConfig.java
+++
@@ -1,7 +0,0 @@
-package com.hangout.hangout.global.config;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-public class WebConfig implements WebMvcConfigurer {
-}
