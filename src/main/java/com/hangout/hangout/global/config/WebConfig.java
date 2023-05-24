f5a2fa6965c9932d51b93bde8d036688e4883566
BellYun <whd3558@naver.com>
2023-05-24 11:27:47 +0900

    refactor : create read update 리팩토링

---
+++ src/main/java/com/hangout/hangout/global/config/WebConfig.java
@@ -0,0 +1,7 @@
+package com.hangout.hangout.global.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+public class WebConfig implements WebMvcConfigurer {
+}
