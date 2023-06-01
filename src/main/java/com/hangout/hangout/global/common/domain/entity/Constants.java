ce3bc1235edee323a0f5b743cbbec8a3e15488d9
OhHongGi <5ghdrl@naver.com>
2023-06-01 16:14:20 +0900

    chore(Constants): Remove the unnecessary constant

--- src/main/java/com/hangout/hangout/global/common/domain/entity/Constants.java
+++ src/main/java/com/hangout/hangout/global/common/domain/entity/Constants.java
@@ -4,9 +4,8 @@ public class Constants {
 
     public final static String API_PREFIX = "/api/v1";
     public final static String AUTHORIZATION_ENDPOINT = API_PREFIX + "/auth/oauth2/authorize";
-    public final static String FAILURE_ENDPOINT = "/login/fail";
+    public final static String FAILURE_ENDPOINT = "/login/oauth2/fail";
     public final static String FAILURE_REDIRECT = "http://localhost:8080" + API_PREFIX + "/auth" + FAILURE_ENDPOINT;
-    public final static String GOOGLE_TOKEN_REQUEST_URL = "https://oauth2.googleapis.com/token";
 
     public final static String[] SWAGGER_URI_LIST = {
         "/api/v1/auth/**","/",
