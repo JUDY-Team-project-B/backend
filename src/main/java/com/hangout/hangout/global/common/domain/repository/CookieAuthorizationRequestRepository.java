5d424bc913a2ce5892efeeb7bd3d3512bca9fe28
OhHongGi <5ghdrl@naver.com>
2023-06-05 01:23:16 +0900

    docs(CookieAuthorizationRequestRepository): Add comments
    Method
    - loadAuthorizationRequest
    - saveAuthorizationRequest
    - removeAuthorizationRequestCookies

--- src/main/java/com/hangout/hangout/global/common/domain/repository/CookieAuthorizationRequestRepository.java
+++ src/main/java/com/hangout/hangout/global/common/domain/repository/CookieAuthorizationRequestRepository.java
@@ -20,6 +20,12 @@ public class CookieAuthorizationRequestRepository implements AuthorizationReques
     public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect-uri";
     private static final int COOKIE_EXPIRE_SECONDS = 180;
 
+    /**
+     * 처음 social login uri로 요청 시, 소셜 로그인 페이지로 redirect와 동시에 인증 요청임을 증명하기 위해 Cookie에 저장
+     * (이 과정을 거치지 않는 경우, 인증 요청 실패 및 OAuth2AuthenticationFailureHandler의 처리 진행 )
+     * @param request the {@code HttpServletRequest}
+     * @return OAuth2AuthorizationRequest
+     */
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
         return CookieUtil.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
@@ -27,6 +33,12 @@ public class CookieAuthorizationRequestRepository implements AuthorizationReques
             .orElse(null);
     }
 
+    /**
+     * 처음 social login uri로 요청을 진행할 때, redirect-uri param 값을 Cookie에 저장하며, 이는 추후 소셜 로그인이
+     * 정상적으로 진행된 후 OAuth2AuthenticationSuccessHandler 처리 과정에서 redirect될 uri를 설정하는 과정에서
+     * 해당 Cookie가 사용됨
+     * @param request the {@code HttpServletRequest}
+     */
     @Override
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest,
         HttpServletRequest request, HttpServletResponse response) {
@@ -50,6 +62,11 @@ public class CookieAuthorizationRequestRepository implements AuthorizationReques
         return this.loadAuthorizationRequest(request);
     }
 
+    /**
+     * request, response의 인가 요청 및 redirect-uri cookie 정보를 삭제 처리
+     * @param request the {@code HttpServletRequest}
+     * @param response the {@code HttpServletResponse}
+     */
     public void removeAuthorizationRequestCookies(HttpServletRequest request,
         HttpServletResponse response) {
         CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
