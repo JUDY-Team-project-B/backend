ce44972da52282ee0e80985df15905d3a5d94bee
OhHongGi <5ghdrl@naver.com>
2023-07-05 18:33:18 +0900

    build(RedisConfig): Set the redis configuration

---
+++ src/main/java/com/hangout/hangout/global/common/domain/entity/CacheKey.java
@@ -0,0 +1,10 @@
+package com.hangout.hangout.global.common.domain.entity;
+
+import lombok.Getter;
+
+@Getter
+public class CacheKey {
+
+    public final static Long DEFAULT_EXPIRED_TIME = 60L;
+
+}
