a3f058844a591b06d5e1f59bc4151d2aefdec303
OhHongGi <5ghdrl@naver.com>
2023-07-24 13:46:23 +0900

    chore(CurrentUser): Add JsonIgnore annotation for swagger test

--- src/main/java/com/hangout/hangout/global/security/CurrentUser.java
+++ src/main/java/com/hangout/hangout/global/security/CurrentUser.java
@@ -1,5 +1,6 @@
 package com.hangout.hangout.global.security;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? new com.hangout.hangout.domain.user.entity.EmptyUser() : user")
+@JsonIgnore
 public @interface CurrentUser {
 
 }
