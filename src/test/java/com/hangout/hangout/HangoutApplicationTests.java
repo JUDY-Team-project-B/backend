1165bf1e3cdcd1bbc0dac2c7ea609695702272bc
OhHongGi <5ghdrl@naver.com>
2023-07-31 20:56:11 +0900

    fix(Remove test code):
    Add removed code after setting h2 database

--- src/test/java/com/hangout/hangout/HangoutApplicationTests.java
+++ src/test/java/com/hangout/hangout/HangoutApplicationTests.java
@@ -1,13 +1,8 @@
 package com.hangout.hangout;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HangoutApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
--- src/test/java/com/hangout/hangout/domain/post/repository/PostRepositoryQuerydslImplTest.java
+++ src/test/java/com/hangout/hangout/domain/post/repository/PostRepositoryQuerydslImplTest.java
@@ -1,39 +1,39 @@
-package com.hangout.hangout.domain.post.repository;
-
-import com.hangout.hangout.global.config.TestConfig;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.PageRequest;
-
-@Import(TestConfig.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-class PostRepositoryQuerydslImplTest {
-
-    @Autowired
-    private PostRepository postRepository;
-
-    // 게시물을 조회 수가 많은 순으로 조회
-    @Test
-    void findAllByOrderByPostHits() {
-        PageRequest page = PageRequest.of(0, 8);
-        boolean isDescending = true;
-
-        postRepository.findAllByOrderByPostHits(page,
-            isDescending).getContent();
-    }
-
-    // 게시물을 좋아요 수가 많은 순으로 조회
-    @Test
-    void findAllByOrderByPostLikes() {
-        PageRequest page = PageRequest.of(0, 8);
-        boolean isDescending = true;
-
-        postRepository.findAllByOrderByPostLikes(page, isDescending);
-    }
-}
-
+//package com.hangout.hangout.domain.post.repository;
+//
+//import com.hangout.hangout.global.config.TestConfig;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.data.domain.PageRequest;
+//
+//@Import(TestConfig.class)
+//@DataJpaTest
+//@AutoConfigureTestDatabase
+//class PostRepositoryQuerydslImplTest {
+//
+//    @Autowired
+//    private PostRepository postRepository;
+//
+//    // 게시물을 조회 수가 많은 순으로 조회
+//    @Test
+//    void findAllByOrderByPostHits() {
+//        PageRequest page = PageRequest.of(0, 8);
+//        boolean isDescending = true;
+//
+//        postRepository.findAllByOrderByPostHits(page,
+//            isDescending).getContent();
+//    }
+//
+//    // 게시물을 좋아요 수가 많은 순으로 조회
+//    @Test
+//    void findAllByOrderByPostLikes() {
+//        PageRequest page = PageRequest.of(0, 8);
+//        boolean isDescending = true;
+//
+//        postRepository.findAllByOrderByPostLikes(page, isDescending);
+//    }
+//}
+//
