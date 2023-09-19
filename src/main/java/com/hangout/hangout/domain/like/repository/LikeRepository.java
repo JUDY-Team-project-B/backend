c5c6acd66fa5a4bbfa5ba55dab2ab0ce97726ff4
조성현 <agllwy116@naver.com>
2023-09-19 10:55:01 +0900

    feat : 게시글 좋아요 Repository Querydsl 형식으로 다시 작성

--- src/main/java/com/hangout/hangout/domain/like/repository/LikeRepository.java
+++ src/main/java/com/hangout/hangout/domain/like/repository/LikeRepository.java
@@ -1,22 +1,11 @@
 package com.hangout.hangout.domain.like.repository;
 
 import com.hangout.hangout.domain.like.entity.PostLike;
-import com.hangout.hangout.domain.post.entity.Post;
-import com.hangout.hangout.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface LikeRepository extends JpaRepository<PostLike, Long> {
-    Optional<PostLike> findByUserAndPost(User user, Post post);
-    List<PostLike> findAllByUserId(Long userId);
+public interface LikeRepository extends JpaRepository<PostLike, Long>, LikeRepositoryQuerydsl {
 
-    @Modifying
-    @Query("DELETE FROM PostLike pl WHERE pl.user = :user AND pl.post = :post")
-    void deletePostLikeByUserAndPost(User user, Post post);
 }
